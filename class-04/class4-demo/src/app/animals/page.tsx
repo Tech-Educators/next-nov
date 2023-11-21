1102775f12fb3365b1f5b1a029d3264761989609
Sam Louloudis <samanthaleelouloudis@gmail.com>
2023-11-21 11:14:28 +0000

    i love talking!

--- class-04/class4-demo/src/app/animals/page.tsx
+++ class-04/class4-demo/src/app/animals/page.tsx
@@ -1,5 +1,15 @@
 import Link from 'next/link'
 
+type petsSearchQuery = {
+    sortBy: string
+}
+
+// FunkyNextObject = {
+//     searchParms: {
+//         sortBy: 'Whatever we do with ?sortBy=something'
+//     }
+// }
+
 type petsType = {
     name: string,
     id: number
@@ -26,14 +36,46 @@ let pets: petsType[] = [{
     }
 ]
 
-export default function Page() {
+// this compares each object in our pets array. If object a.name is < object b.name, it will swap them in the array. 
+// but don't worry about this too much: to quote Tod, it just works. 
+function comparePets(a: petsType, b: petsType) {
+    if (a.name < b.name) {
+        return -1;
+    } else if (a.name > b.name) {
+        return 1;
+    } else {
+        return 0
+    }
+}
+
+// next js 'exposes' the search Params to us (search params being the ?query=something in URLS). We're destructing that and then typing it. 
+export default function Page({searchParams} : {searchParams: petsSearchQuery}) {
+
+    // making a copy of the pets array so we don't mutate it. (if we just use the pets, it makes it impossible to 'undo' any sorting, even when there are no serach params)
+    let sortedPets = [...pets]
+
+    if (searchParams.sortBy == 'asc') {
+        sortedPets.sort(comparePets)
+    } else if (searchParams.sortBy == 'desc') {
+        sortedPets.sort(comparePets).reverse()
+    }
+
     return (
         <div>
             <h4>This is a website about animals: here are many animals!</h4>
-            {pets.map(pet => {
+            <p>{searchParams.sortBy}</p>
+            <br />
+            <Link href='/animals'>Remove the sort</Link>
+            <br />
+            <Link href='/animals?sortBy=asc'>Sort by ascending</Link>
+            <br />
+            <Link href='/animals?sortBy=desc'>Sort by descending</Link>
+            <br />
+            <br />
+            <br />
+            {sortedPets.map(pet => {
                 return (
                     <div key={pet.id}>
-                        <br />
                         <Link href={`/animals/${pet.name.toLowerCase()}`}>{pet.name}</Link>
                     </div>
                 )
