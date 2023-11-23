62cb4b78ea1c6746aa71bc8824d15037f25bae7a
Sam Louloudis <samanthaleelouloudis@gmail.com>
2023-11-23 13:44:43 +0000

    class9

---
+++ class-08/deployed-blog/src/app/blogs/[slug]/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+        <div>
+            <h1>This is the post title</h1>
+            <p>This is the post content</p>
+        </div>
+    )
+}
---
+++ class-08/deployed-blog/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>This is the home page. The root route. </h1>
+      <h2>This is a change to the website!</h2>
+    </main>
+  )
+}
